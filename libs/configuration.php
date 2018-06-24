e43d7330fa6c9e3e2ce8d9c5c0a66781c3246044
Diego2696 <dfonseca2696@gmail.com>
2018-06-23 20:21:31 -0600

    a

--- libs/configuration.php
+++ libs/configuration.php
@@ -6,17 +6,7 @@ $config->set('controllerFolder', 'controller/');
 $config->set('modelFolder', 'model/');
 $config->set('viewFolder', 'view/');
 
-//$config->set('dbhost', '163.178.107.130');
-//$config->set('dbname', 'db_task1_expert_systems_diego_cedeno_2018');
-//$config->set('dbuser', 'laboratorios');
-//$config->set('dbpass', 'UCRSA.118');
-
 $config->set('dbhost', 'sql9.freemysqlhosting.net');
-$config->set('dbname', 'sql9241265');
-$config->set('dbuser', 'sql9241265');
-$config->set('dbpass', '4UivGEeCx3');
-
-//$config->set('dbhost', 'localhost');
-//$config->set('dbname', 'db_task2_experts_diego_cedeno');
-//$config->set('dbuser', 'root');
-//$config->set('dbpass', 'root');
+$config->set('dbname', 'sql9233493');
+$config->set('dbuser', 'sql9233493');
+$config->set('dbpass', 'DEDXNCMYvV');
